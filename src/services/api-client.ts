54ba8469454ca87f3d010681204cfa39420896ac
Fu Lim <fulim1130@gmail.com>
2024-03-03 22:16:09 +0800

    tested not problem

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiUrl = import.meta.env.VITE_API_URL as string;
-console.log(apiUrl);
+// console.log(apiUrl);
 
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
