e438e9dd50781cdfa95f2f64e90835a4f1149b98
Fu Lim <fulim1130@gmail.com>
2024-03-11 18:43:41 +0800

    update api to link to my own backend to hide the api key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const apiUrl = import.meta.env.VITE_API_URL as string;
+// const apiUrl = import.meta.env.VITE_API_URL as string;
 // console.log(apiUrl);
 
 export default axios.create({
-  baseURL: "https://api.rawg.io/api",
-  headers: {
-    Authorization: `Bearer ${apiUrl}`,
-  },
+  baseURL: "https://gamehub-backend-swart.vercel.app/api",
+  // params: {
+  //   key: "<API-Key>",
+  // },
 });
