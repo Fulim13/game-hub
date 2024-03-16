0f5e0a389c823828be9822fe7e1d520d11b06909
Fu Lim <fulim1130@gmail.com>
2024-03-16 23:27:41 +0800

    Refactor- simplify game query

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -4,11 +4,17 @@ import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 
 interface Props {
   onSelectPlatform: (platform: Platform) => void;
-  selectedPlatform: Platform | null;
+  selectedPlatformId?:number;
 }
 
-const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
+const PlatformSelector = ({
+  onSelectPlatform,
+  selectedPlatformId,
+}: Props) => {
   const { data, error } = usePlatforms();
+  const selectedPlatform = data?.results.find(
+    (platform) => platform.id === selectedPlatformId
+  );
 
   if (error) return null;
   return (
