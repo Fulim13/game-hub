638df882d620de0b80c78399325e49a2f7ce9993
Fu Lim <fulim1130@gmail.com>
2024-03-16 18:30:50 +0800

    Remove comment on FetchResponse interface

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -2,10 +2,10 @@ import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
 import { AxiosRequestConfig, CanceledError } from "axios";
 
-// interface FetchResponse<T> {
-//   count: number;
-//   results: T[];
-// }
+interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
 
 const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {
   const [data, setData] = useState<T[]>([]);
@@ -17,13 +17,12 @@ const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?:
 
     setLoading(true);
     apiClient
-      // .get<FetchResponse<T>>(endpoint, {
-      .get(endpoint, {
+      .get<FetchResponse<T>>(endpoint, {
         signal: controller.signal,
         ...requestConfig,
       })
       .then((res) => {
-        setData(res.data);
+        setData(res.data.results);
         setLoading(false);
       })
       .catch((err) => {
